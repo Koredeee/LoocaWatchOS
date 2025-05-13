7b1e44678910222bc9f585548ccecaec291b5342
Koredeee <muhammad.fawwaz003@binus.ac.id>
2025-05-13 18:55:58 +0700

    Initial Commit

---
+++ LoocaWatchOS Watch App/LoocaWatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoocaWatchOSApp.swift
+//  LoocaWatchOS Watch App
+//
+//  Created by FWZ on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LoocaWatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
